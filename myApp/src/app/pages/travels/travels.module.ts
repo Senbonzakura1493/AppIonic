08d78c89ea57a2ba4b63bc62bcbe8a876a6c14e6
Senbonzakura1493 <fatine.benmoussa@icloud.com>
2019-06-01 22:59:40 +0200

    side menu added

---
+++ myApp/src/app/pages/travels/travels.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { TravelsPage } from './travels.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TravelsPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [TravelsPage]
+})
+export class TravelsPageModule {}
