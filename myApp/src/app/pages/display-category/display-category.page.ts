a6cafe46ac139336ad3c0520190d71940a4720c6
Senbonzakura1493 <fatine.benmoussa@icloud.com>
2019-06-02 11:57:31 +0200

    delete toast message

--- myApp/src/app/pages/display-category/display-category.page.ts
+++ myApp/src/app/pages/display-category/display-category.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import {CategoryServiceService} from 'src/app/services/category.service'
 import { Category } from 'src/app/classes/category';
+import { ToastController } from '@ionic/angular'; 
 
 @Component({
   selector: 'app-display-category',
@@ -14,7 +15,7 @@ export class DisplayCategoryPage implements OnInit {
 
   constructor(private route: ActivatedRoute, 
               private categoryService: CategoryServiceService, 
-              private router: Router) { }
+              private router: Router,public toastController: ToastController) { }
 
   ngOnInit() {
     this.categoryId = Number(this.route.snapshot.paramMap.get('id'));
@@ -22,8 +23,34 @@ export class DisplayCategoryPage implements OnInit {
   }
   
   delete(id: number) {
+    try 
+    {
     this.categoryService.deleteCategory(id);
     this.router.navigateByUrl('categories');
+    this.succesMessage();
+    }
+    catch{
+      this.failMessage();
+    }
+
   }
 
+   /*toast messages*/
+   async succesMessage() {    
+    const toast = await this.toastController.create({      
+       message: 'Successfully deleted!',       
+       duration: 2000,       
+       color: "success"     
+      });     
+    toast.present();   
+  }
+  
+  async failMessage() {    
+    const toast = await this.toastController.create({      
+       message: 'Oups! Could not delete the category ! Verify your entries ',       
+       duration: 4000,       
+       color: "danger"     
+      });     
+    toast.present();   
+  }
 }
--- myApp/src/app/pages/display-travel/display-travel.page.ts
+++ myApp/src/app/pages/display-travel/display-travel.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import {TravelServiceService} from 'src/app/services/travel.service'
 import { Travel } from 'src/app/classes/travel';
+import { ToastController } from '@ionic/angular'; 
 
 @Component({
   selector: 'app-display-travel',
@@ -14,7 +15,7 @@ export class DisplayTravelPage implements OnInit {
 
   constructor(private route: ActivatedRoute, 
               private travelService: TravelServiceService, 
-              private router: Router) { }
+              private router: Router, public toastController: ToastController) { }
 
   ngOnInit() {
     this.travId = Number(this.route.snapshot.paramMap.get('id'));
@@ -22,8 +23,33 @@ export class DisplayTravelPage implements OnInit {
   }
   
   delete(id: number) {
-    this.travelService.deleteTravel(id);
-    this.router.navigateByUrl('travels');
+    try {
+      this.travelService.deleteTravel(id);
+      this.router.navigateByUrl('travels');
+      this.succesMessage();
+    }
+    catch{
+      this.failMessage();
+    }
+  }
+
+  /*toast messages*/
+  async succesMessage() {    
+    const toast = await this.toastController.create({      
+       message: 'Successfully deleted!',       
+       duration: 2000,       
+       color: "success"     
+      });     
+    toast.present();   
+  }
+  
+  async failMessage() {    
+    const toast = await this.toastController.create({      
+       message: 'Oups! Could not delete the travel ! Verify your entries ',       
+       duration: 4000,       
+       color: "danger"     
+      });     
+    toast.present();   
   }
 
 }
