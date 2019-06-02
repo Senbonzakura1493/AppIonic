1199efaba9addfac1f7ad3971876cc886a66cf45
Senbonzakura1493 <fatine.benmoussa@icloud.com>
2019-06-02 09:55:20 +0200

    toast messages for category

--- myApp/src/app/pages/create-category/create-category.page.ts
+++ myApp/src/app/pages/create-category/create-category.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Category } from 'src/app/classes/category';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CategoryServiceService } from 'src/app/services/category.service';
+import { ToastController } from '@ionic/angular'; 
 
 @Component({
   selector: 'app-create-category',
@@ -13,13 +14,40 @@ export class CreateCategoryPage implements OnInit {
   public newCategory = new Category(this.categoryService.Id + 1, '', '');
   constructor(private route: ActivatedRoute, 
     private categoryService: CategoryServiceService, 
-    private router: Router) { }
+    private router: Router, public toastController: ToastController) { }
 
   ngOnInit() {
   }
 
   add() {
-    this.categoryService.postCategory(this.newCategory);
-    this.router.navigateByUrl('home/categories');
+
+    if (this.newCategory.name !== undefined && this.newCategory.name.length !== 0){
+      this.categoryService.postCategory(this.newCategory);
+      this.router.navigateByUrl('home/categories');
+      this.succesMessage();
+    }
+    else{
+        this.failMessage();
+    }
+    
   }
+
+  /*toast messages*/
+  async succesMessage() {    
+    const toast = await this.toastController.create({      
+       message: 'Category added successfully !',       
+       duration: 2000,       
+       color: "success"     
+      });     
+    toast.present();   
+  }
+  async failMessage() {    
+    const toast = await this.toastController.create({      
+       message: 'Oups! Could not add the category ! Verify your entries ',       
+       duration: 4000,       
+       color: "danger"     
+      });     
+    toast.present();   
+  }
+  
 }
--- myApp/src/app/pages/modify-category/modify-category.page.ts
+++ myApp/src/app/pages/modify-category/modify-category.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Category } from 'src/app/classes/category';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CategoryServiceService } from 'src/app/services/category.service';
+import { ToastController } from '@ionic/angular'; 
+
 @Component({
   selector: 'app-modify-category',
   templateUrl: './modify-category.page.html',
@@ -15,7 +17,7 @@ export class ModifyCategoryPage implements OnInit {
 
   constructor(private route: ActivatedRoute, 
     private categoryService: CategoryServiceService, 
-    private router: Router) { }
+    private router: Router, public toastController: ToastController) { }
 
   ngOnInit() {
     this.categoryId = Number(this.route.snapshot.paramMap.get('id'));
@@ -23,11 +25,39 @@ export class ModifyCategoryPage implements OnInit {
   }
 
   modify() {
-    this.updatedCategory = new Category(this.category.id, 
-                                        this.category.name, 
-                                        this.category.description);
-    this.categoryService.putCategory(this.updatedCategory);
-    this.router.navigateByUrl('categories');
+
+      this.updatedCategory = new Category(this.category.id, 
+                                          this.category.name, 
+                                          this.category.description);
+      if (this.updatedCategory.name !== undefined && this.updatedCategory.name !== "" &&
+          this.updatedCategory.description !== undefined && this.updatedCategory.description !== ""
+          )
+        
+        {
+        this.categoryService.putCategory(this.updatedCategory);
+        this.router.navigateByUrl('categories');
+        this.succesMessage();
+      }
+      else{
+        this.failMessage();
+      }
   }
 
+  /*toast messages*/
+  async succesMessage() {    
+    const toast = await this.toastController.create({      
+       message: 'Successfully modified!',       
+       duration: 2000,       
+       color: "success"     
+      });     
+    toast.present();   
+  }
+  async failMessage() {    
+    const toast = await this.toastController.create({      
+       message: 'Oups! Could not modify the category ! Verify your entries ',       
+       duration: 4000,       
+       color: "danger"     
+      });     
+    toast.present();   
+  }
 }
